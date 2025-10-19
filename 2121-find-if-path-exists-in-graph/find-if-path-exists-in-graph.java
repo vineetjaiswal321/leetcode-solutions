99634419c6000fcb656309b28908658df5c2ed5b
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-10-19 23:15:44 +0530

    Time: 161 ms (31.91%) | Memory: 152.4 MB (67.58%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,15 +1,7 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++)    adj.add(new ArrayList<>());
-        for(int[] edge : edges)
-        {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
-        }
-
+    public void bfs(int source, boolean[] visted, List<List<Integer>> adj)
+    {
         Queue<Integer> q=new LinkedList<>();
-        boolean[] visted=new boolean[n];
         q.add(source);
         visted[source]=true;
         while(q.size()>0)
@@ -24,6 +16,17 @@ class Solution {
                 }   
             }
         }
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++)    adj.add(new ArrayList<>());
+        for(int[] edge : edges)
+        {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        boolean[] visted=new boolean[n];
+        bfs(source, visted, adj);
         return visted[destination];
     }
 }
