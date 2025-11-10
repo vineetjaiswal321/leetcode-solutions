79b43309a0505445dae39046f0d44b6e5e0599f2
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-11-10 18:10:12 +0530

    Time: 3 ms (32.28%) | Memory: 49.2 MB (5.86%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,47 +1,37 @@
 class Solution {
-    public void bfs(int i, boolean[] visited, int[][] isConnected)
-    {
-        Queue<Integer> q=new LinkedList<>();
-        int n=isConnected.length;
-        q.add(i);
-        visited[i]=true;
-        while(q.size()>0)
-        {
-            int front=q.remove();
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[front][j]==1 && !visited[j])
-                {
-                    q.add(j);
-                    visited[j]=true;
-                }
-            }
-        }
-    }
-    public void dfs(int i, boolean[] visited, int[][] isConnected)
-    {
-        int n=isConnected.length;
-        visited[i]=true;
-        for(int j=0;j<n;j++)
-        {
-            if(isConnected[i][j]==1 && visited[j]==false)
-            {
-                dfs(j, visited, isConnected);
+    public void dfs(int node, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
+        vis[node]=true;
+
+        for(int i : adj.get(node)){
+            if(!vis[i]){
+                dfs(i, vis, adj);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length, count=0;
-        boolean[] visited=new boolean[n];
+        int n=isConnected.length;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++)    adj.add(new ArrayList<>());
+
         for(int i=0;i<n;i++)
         {
-            if(!visited[i])
-            {
-                //bfs(i, visited, isConnected);
-                dfs(i, visited, isConnected);
-                count++;
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
             }
         }
-        return count;
+        int cnt=0;
+        boolean[] vis=new boolean[n];
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i]){
+                dfs(i, vis, adj);
+                cnt++;
+            }
+        }
+        return cnt;
+
     }
 }
