8a7da34f46fccd9fd3233a410b71e3a6ae2e08d0
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-11-11 23:30:35 +0530

    Time: 2 ms (9.26%) | Memory: 47 MB (5.77%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,4 +1,12 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
     public void dfs(int[][] image, int sr, int sc, boolean[][] vis, int color, int ic){
         int m=image.length, n=image[0].length;
         vis[sr][sc]=true;
@@ -16,11 +24,36 @@ class Solution {
             }
         }
     }
+    public void bfs(int[][] image, int sr, int sc, boolean[][] vis, int color, int ic){
+        int m=image.length, n=image[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(sr, sc));
+        vis[sr][sc]=true;
+        while(q.size()>0){
+            Pair p=q.poll();
+            int row=p.row;
+            int col=p.col;
+            image[row][col]=color;
+            for(int delrow=-1;delrow<=1;delrow++){
+                for(int delcol=-1;delcol<=1;delcol++){
+                    if(Math.abs(delrow) + Math.abs(delcol)!=1)  continue;
+                    int newRow=row+delrow;
+                    int newCol=col+delcol;
+                    if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && image[newRow][newCol]==ic && !vis[newRow][newCol])
+                    {
+                        q.add(new Pair(newRow, newCol));
+                        vis[newRow][newCol]=true;
+                    }
+                }
+            }
+        }
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int ic=image[sr][sc];
         int m=image.length, n=image[0].length;
         boolean[][] vis=new boolean[m][n];
-        dfs(image, sr, sc, vis, color, ic);
+        // dfs(image, sr, sc, vis, color, ic);
+        bfs(image, sr, sc, vis, color, ic);
         return image;
     }
 }
