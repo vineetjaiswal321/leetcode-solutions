bce7e4d314fbf8abfd2b782956f32bd0719bedfe
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-11-12 20:35:54 +0530

    Time: 15 ms (35.12%) | Memory: 49 MB (13.86%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        int dis;
+        Pair(int row, int col, int dis){
+            this.row=row;
+            this.col=col;
+            this.dis=dis;
+        }
+    }
+    public int[][] updateMatrix(int[][] mat) {
+        int m=mat.length, n=mat[0].length;
+        boolean[][] vis=new boolean[m][n];
+        Queue<Pair> q=new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    q.add(new Pair(i, j, 0));
+                    vis[i][j]=true;
+                }
+            }
+        }
+
+        while(q.size()>0){
+            Pair p=q.poll();
+            int row=p.row;
+            int col=p.col;
+            int dis=p.dis;
+
+            for(int delrow=-1;delrow<=1;delrow++){
+                for(int delcol=-1;delcol<=1;delcol++){
+                    if(Math.abs(delrow) + Math.abs(delcol)!=1)  continue;
+                    int newRow=row+delrow;
+                    int newCol=col+delcol;
+                    if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !vis[newRow][newCol] && mat[newRow][newCol]==1){
+                        q.add(new Pair(newRow, newCol, dis+1));
+                        mat[newRow][newCol]=dis+1;
+                        vis[newRow][newCol]=true;
+                    }
+                }
+            }
+        }
+        return mat;
+    }
+}
