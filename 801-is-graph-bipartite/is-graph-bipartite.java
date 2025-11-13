e50cd4cf5df233a239f7a2a9156fc0f6599767fe
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-11-13 23:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (12.03%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -18,6 +18,20 @@ class Solution {
         }
         return true;
     }
+    public boolean dfs(int in, int currColor, int[][] graph, int[] color){
+        color[in]=currColor;
+
+        for(int i : graph[in]){
+            if(color[i]==-1){
+                int newColor=(currColor==0) ? 1 : 0;
+                if(!dfs(i, newColor, graph, color)) return false;
+            }
+            else if(color[i]==currColor){
+                return false;
+            }
+        }
+        return true;
+    }
     public boolean isBipartite(int[][] graph) {
         int m=graph.length;
         int[] color=new int[m];
@@ -25,7 +39,8 @@ class Solution {
 
         for(int i=0;i<m;i++){
             if(color[i]==-1){
-                if(!bfs(i, graph, color))   return false;
+                // if(!bfs(i, graph, color))   return false;
+                if(!dfs(i, 0, graph, color))   return false;
             }
         }
         return true;
