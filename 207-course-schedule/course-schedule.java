ecfa86bda7d68826a2cab54e4186434aaff1a52f
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-11-15 16:52:11 +0530

    Time: 7 ms (48.55%) | Memory: 47.8 MB (5.56%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,19 +1,4 @@
 class Solution {
-    public boolean dfs(int in, boolean[] vis, boolean[] pathVis, Stack<Integer> st, ArrayList<ArrayList<Integer>> adj){
-        vis[in]=true;
-        pathVis[in]=true;
-        for(int i : adj.get(in)){
-            if(!vis[i]){
-                if(dfs(i, vis, pathVis, st, adj))    return true;
-            }
-            else if(pathVis[i]) return true;
-        }
-
-        pathVis[in]=false;
-        st.add(in);
-        st.add(in);//1 0
-        return false;
-    }
     public boolean canFinish(int V, int[][] edges) {
         //Topological Sort
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
@@ -21,23 +6,29 @@ class Solution {
         for(int[] edge : edges){
             adj.get(edge[0]).add(edge[1]);
         }
-
-        //DFS
-        boolean[] vis=new boolean[V];
-        boolean[] pathVis=new boolean[V];
-        Stack<Integer> st=new Stack<>();
-
+        
+        int[] inDegree=new int[V];
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i, vis, pathVis, st, adj))   return false;
+            for(int j : adj.get(i)){
+                inDegree[j]+=1;
             }
         }
-        return true;
-        // ArrayList<Integer> topo=new ArrayList<>();
-        // while(st.size()>0){
-        //     topo.add(st.pop());
-        // }
+        //BFS
+        Queue<Integer> q=new LinkedList<>();
+        ArrayList<Integer> topo=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            if(inDegree[i]==0)  q.add(i);
+        }
 
-        // return !(topo.size()==V);   // if topo size equals V then there is no cycle 
+        while(q.size()>0){
+            int node=q.poll();  
+            topo.add(node);
+            for(int i : adj.get(node)){
+                inDegree[i]-=1;
+                if(inDegree[i]==0)  q.add(i);
+            }
+        }
+
+        return (topo.size()==V);
     }
 }
