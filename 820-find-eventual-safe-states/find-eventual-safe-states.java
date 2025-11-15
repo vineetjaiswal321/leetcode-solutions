be229276722bad4ca264cdbb7144c8d3cfa95561
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2025-11-15 18:22:05 +0530

    Time: 25 ms (32.79%) | Memory: 60.4 MB (18.95%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,41 +1,33 @@
 class Solution {
-    public boolean dfsCheck(int in, int[][] graph, boolean[] vis, boolean[] pathVis, boolean[] check){
-        vis[in]=true;
-        pathVis[in]=true;
-
-        for(int i : graph[in]){
-            if(!vis[i]){
-                if(dfsCheck(i, graph, vis, pathVis, check)){
-                    check[in]=true;
-                    return true;
-                }
-            }
-            else if(pathVis[i]){
-                check[in]=true;
-                return true;
-            }
-        }
-        pathVis[in]=false;
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n=graph.length;
-        List<Integer> safeNode=new ArrayList<>();
-
-        boolean[] vis=new boolean[n];
-        boolean[] pathVis=new boolean[n];
-        boolean[] check=new boolean[n]; //notSafe
+        ArrayList<ArrayList<Integer>> adjR=new ArrayList<>();
+        for(int i=0;i<n;i++)    adjR.add(new ArrayList<>());
+        int[] inDegree=new int[n];
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfsCheck(i, graph, vis, pathVis, check);
+            for(int j : graph[i]){
+                adjR.get(j).add(i);
+                inDegree[i]+=1;
             }
         }
 
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(!check[i]){   //false -> safe
-                safeNode.add(i);
+            if(inDegree[i]==0)  q.add(i);
+        }
+        ArrayList<Integer> topo=new ArrayList<>();
+        while(q.size()>0){
+            int node=q.poll();
+            topo.add(node);
+
+            for(int i : adjR.get(node)){
+                inDegree[i]-=1;
+                if(inDegree[i]==0){
+                    q.add(i);
+                }
             }
         }
-        return safeNode;
+        Collections.sort(topo);
+        return topo;
     }
 }
