2c39ec45034093e619c26b9d891995209c1b2860
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2026-02-07 22:18:00 +0530

    Time: 1 ms (14.16%) | Memory: 43.2 MB (47.18%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,13 +14,22 @@
  * }
  */
 class Solution {
-    //left right root
     List<Integer> li;
     public void postOrder(TreeNode root){
         if(root==null)  return;
-        postOrder(root.left);
-        postOrder(root.right);
-        li.add(root.val);
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
+        st1.push(root);
+        while(!st1.isEmpty()){
+            TreeNode node=st1.pop();
+            if(node.left!=null)    st1.add(node.left);
+            if(node.right!=null)    st1.add(node.right);
+            st2.push(node);
+        }
+
+        while(!st2.isEmpty()){
+            li.add(st2.pop().val);
+        }
     }
     public List<Integer> postorderTraversal(TreeNode root) {
         li=new ArrayList<>();
