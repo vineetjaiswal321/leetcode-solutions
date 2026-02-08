b12c69d534d57f6b4719e99aedbd4ca9ddba83bc
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2026-02-08 16:51:56 +0530

    Time: 1 ms (7.45%) | Memory: 43.2 MB (67.53%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,27 +13,60 @@
  *     }
  * }
  */
+class Pair{
+    TreeNode node;
+    int num;
+    Pair(TreeNode node, int num){
+        this.node=node;
+        this.num=num;
+    }
+}
 class Solution {
     //root left right
-    List<Integer> li;
+    List<Integer> pre;
+    List<Integer> in;
+    List<Integer> post;
     public void preOrder(TreeNode root){
         if(root==null)  return;
         // li.add(root.val);
         // preOrder(root.left);
         // preOrder(root.right);
+        // Stack<TreeNode> st=new Stack<>();
+        // st.push(root);
+        // while(!st.isEmpty()){
+        //     TreeNode t=st.pop();
+        //     li.add(t.val);
+        //     if(t.right!=null)   st.push(t.right);
+        //     if(t.left!=null)   st.push(t.left);
+        // }
 
-        Stack<TreeNode> st=new Stack<>();
-        st.push(root);
+        Stack<Pair> st=new Stack<>();
+        st.push(new Pair(root, 1));
         while(!st.isEmpty()){
-            TreeNode t=st.pop();
-            li.add(t.val);
-            if(t.right!=null)   st.push(t.right);
-            if(t.left!=null)   st.push(t.left);
+            Pair it=st.pop();
+            if(it.num==1){
+                it.num+=1;
+                pre.add(it.node.val);
+                st.push(it);
+                if(it.node.left!=null)  st.push(new Pair(it.node.left, 1));
+            }
+            else if(it.num==2){
+                it.num+=1;
+                in.add(it.node.val);
+                st.push(it);
+                if(it.node.right!=null)
+                st.push(new Pair(it.node.right, 1));
+            }
+            else{
+                post.add(it.node.val);
+            }
         }
     }
     public List<Integer> preorderTraversal(TreeNode root) {
-        li=new ArrayList<>();
+        pre=new ArrayList<>();
+        in = new ArrayList<>();
+        post = new ArrayList<>();
         preOrder(root);
-        return li;
+        return pre;
     }
 }
