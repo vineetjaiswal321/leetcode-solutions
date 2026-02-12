69e72c22e0321f55e0dcc95d23b105ce87cabf42
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2026-02-12 19:47:21 +0530

    Time: 13 ms (47.69%) | Memory: 44.3 MB (47.18%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        Map<TreeNode, TreeNode> mp=new HashMap<>();
+        mp.put(root, null);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                if(node.left!=null){
+                    mp.put(node.left, node);
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    mp.put(node.right, node);
+                    q.offer(node.right);
+                }
+            }
+        }
+        Set<TreeNode> isVisted=new HashSet<>();
+        q.offer(target);
+        isVisted.add(target);
+        int dis=0;
+        while(true){
+            if(dis==k)  break;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                if(node.left!=null && !isVisted.contains(node.left)){
+                    q.offer(node.left);
+                    isVisted.add(node.left);
+                }
+                if(node.right!=null && !isVisted.contains(node.right)){
+                    q.offer(node.right);
+                    isVisted.add(node.right);
+                }
+                if(mp.get(node)!=null && !isVisted.contains(mp.get(node))){
+                    q.offer(mp.get(node));
+                    isVisted.add(mp.get(node));
+                }
+            }
+            dis+=1;
+        }
+        List<Integer> li=new ArrayList<>();
+        while(!q.isEmpty()){
+            li.add(q.poll().val);
+        }
+        return li;
+    }
+}
