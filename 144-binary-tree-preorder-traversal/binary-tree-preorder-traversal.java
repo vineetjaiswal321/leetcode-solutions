a81e184dc6c337b4e7f9ca8982abd09809b48ab1
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2026-02-14 18:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (92.93%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -40,33 +40,53 @@ class Solution {
         //     if(t.left!=null)   st.push(t.left);
         // }
 
-        Stack<Pair> st=new Stack<>();
-        st.push(new Pair(root, 1));
-        while(!st.isEmpty()){
-            Pair it=st.pop();
-            if(it.num==1){
-                it.num+=1;
-                pre.add(it.node.val);
-                st.push(it);
-                if(it.node.left!=null)  st.push(new Pair(it.node.left, 1));
-            }
-            else if(it.num==2){
-                it.num+=1;
-                in.add(it.node.val);
-                st.push(it);
-                if(it.node.right!=null)
-                st.push(new Pair(it.node.right, 1));
-            }
-            else{
-                post.add(it.node.val);
-            }
-        }
+        // Stack<Pair> st=new Stack<>();
+        // st.push(new Pair(root, 1));
+        // while(!st.isEmpty()){
+        //     Pair it=st.pop();
+        //     if(it.num==1){
+        //         it.num+=1;
+        //         pre.add(it.node.val);
+        //         st.push(it);
+        //         if(it.node.left!=null)  st.push(new Pair(it.node.left, 1));
+        //     }
+        //     else if(it.num==2){
+        //         it.num+=1;
+        //         in.add(it.node.val);
+        //         st.push(it);
+        //         if(it.node.right!=null)
+        //         st.push(new Pair(it.node.right, 1));
+        //     }
+        //     else{
+        //         post.add(it.node.val);
+        //     }
+        // }
     }
     public List<Integer> preorderTraversal(TreeNode root) {
         pre=new ArrayList<>();
-        in = new ArrayList<>();
-        post = new ArrayList<>();
-        preOrder(root);
+        // in = new ArrayList<>();
+        // post = new ArrayList<>();
+        // preOrder(root);
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                pre.add(curr.val);
+                curr=curr.right;
+            }
+            else{
+                TreeNode prev=curr.left;
+                while(prev.right!=null && prev.right!=curr) prev=prev.right;
+                if(prev.right==null){
+                    pre.add(curr.val);
+                    prev.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    prev.right=null;
+                    curr=curr.right;
+                }
+            }
+        }
         return pre;
     }
 }
