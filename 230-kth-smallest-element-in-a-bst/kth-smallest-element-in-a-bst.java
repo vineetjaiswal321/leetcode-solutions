0f904a53c0be1219a1dffa148f805798debc245f
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2026-02-15 15:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (30.47%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -15,18 +15,41 @@
  */
 class Solution {
     int cnt=0, ans=0;
-    public void helper(TreeNode root, int k){
-        if(root==null)  return;
-        helper(root.left, k);
-        cnt+=1;
-        if(cnt==k){
-            ans=root.val;
-            return;
-        }
-        helper(root.right, k);
-    }
+    // public void helper(TreeNode root, int k){
+    //     if(root==null)  return;
+    //     helper(root.left, k);
+    //     cnt+=1;
+    //     if(cnt==k){
+    //         ans=root.val;
+    //         return;
+    //     }
+    //     helper(root.right, k);
+    // }
     public int kthSmallest(TreeNode root, int k) {
-        helper(root, k);
-        return ans;
+        // helper(root, k);
+        // return ans;
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                cnt+=1;
+                if(cnt==k)  return curr.val;
+                curr=curr.right;
+            }
+            else{
+                TreeNode prev=curr.left;
+                while(prev.right!=null && prev.right!=curr) prev=prev.right;
+                if(prev.right==null){
+                    prev.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    prev.right=null;
+                    cnt+=1;
+                    if(cnt==k)  return curr.val;
+                    curr=curr.right;
+                }
+            }
+        }
+        return -1;
     }
 }
