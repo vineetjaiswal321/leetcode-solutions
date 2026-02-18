f6019ba6a00f8ce0b1bacd07b2cb9005641e5090
vineetjaiswal321 <127409525+vineetjaiswal321@users.noreply.github.com>
2026-02-19 01:10:21 +0530

    Time: 3 ms (73.36%) | Memory: 47.7 MB (8.99%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -16,26 +16,28 @@
 class Solution {
     Stack<TreeNode> st1=new Stack<>();
     Stack<TreeNode> st2=new Stack<>();
-    public void BSTIterator1(TreeNode root){
+    public void pushLeft(TreeNode root){
+        //inorder
         for(; root!=null; st1.push(root), root=root.left);
-    }
-    public void BSTIterator2(TreeNode root){
-        for(; root!=null; st2.push(root), root=root.right);
     }
     public int next1(){
         TreeNode curr=st1.pop();
-        BSTIterator1(curr.right);
+        pushLeft(curr.right);
         return curr.val;
+    }
+    public void pushRight(TreeNode root){
+        //reverse inorder
+        for(; root!=null; st2.push(root), root=root.right);
     }
     public int next2(){
         TreeNode curr=st2.pop();
-        BSTIterator2(curr.left);
+        pushRight(curr.left);
         return curr.val;
     }
     public boolean findTarget(TreeNode root, int k) {
-        BSTIterator1(root);
-        BSTIterator2(root);
-        int x=next1(),  y=next2();
+        pushLeft(root);
+        pushRight(root);
+        int x=next1(), y=next2();
         while(x<y){
             if(x+y==k)  return true;
             else if(x+y>k){
